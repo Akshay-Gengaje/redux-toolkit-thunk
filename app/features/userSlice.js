24f64074995ceaa3488cbb0309fe6b207f1cb845
AkshayGengaje <akshay@ai4mtech.com>
2023-07-18 14:38:10 +0530

    updated code

--- app/features/userSlice.js
+++ app/features/userSlice.js
@@ -1,19 +1,22 @@
-const createSlice = require('@reduxjs/toolkit').createSlice;
-const createAsyncThunk = require('@reduxjs/toolkit').createAsyncThunk;
-const axios = require('axios');
+const createSlice = require("@reduxjs/toolkit").createSlice;
+const createAsyncThunk = require("@reduxjs/toolkit").createAsyncThunk;
+const axios = require("axios");
+
 const initialState = {
-  isLoading: true,
+  isLoading: false,
   users: [],
-  error: '',
+  error: "",
 };
 
-const fetchUsers = createAsyncThunk('user/fetchUsers', () => {
-  return axios.get('https://jsonplaceholder.typicode.com/users').then((res) => {
-    res.data.map((user) => user.id);
-  });
+const fetchUsers = createAsyncThunk("user/fetchUsers", () => {
+  return axios.get("https://jsonplaceholder.typicode.com/users")
+    .then((res) => {
+      return res.data.map((user) => user.id);
+    });
 });
+
 const userSlice = createSlice({
-  name: 'user',
+  name: "user",
   initialState,
   extraReducers: (builder) => {
     builder.addCase(fetchUsers.pending, (state) => {
@@ -24,14 +27,12 @@ const userSlice = createSlice({
       state.users = action.payload;
     });
     builder.addCase(fetchUsers.rejected, (state, action) => {
-      (state.isLoading = false),
-        (state.users = []),
-        (state.error = action.error.message);
+      state.isLoading = false;
+      state.users = [];
+      state.error = action.error.message;
     });
   },
 });
 
-//exporting reducers default
 module.exports = userSlice.reducer;
-//exporting fetchUser async functiom
 module.exports.fetchUsers = fetchUsers;
--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-const store = require('./app/store/store.js');
-const fetchUsers = require('./app/features/userSlice').fetchUsers;
+const store = require("./app/store/store.js");
+const fetchUsers = require("./app/features/userSlice").fetchUsers;
 
-console.log('Initial State : ', store.getState());
+console.log("Initial State : ", store.getState());
 const unsubscribe = store.subscribe(() => {
-  console.log('Updated State : ', store.getState());
+  console.log("Updated State : ", store.getState());
 });
 
-store.dispatch(fetchUsers());
-store.dispatch(fetchUsers());
-
-unsubscribe();
+store.dispatch(fetchUsers())
+  .then(() => {
+    // Dispatch completed
+    unsubscribe();
+  })
+  .catch((error) => {
+    console.error("Failed to fetch users:", error);
+    unsubscribe();
+  });
